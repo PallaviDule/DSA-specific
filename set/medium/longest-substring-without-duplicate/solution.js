a828a329c8f3eed4c1bd6766ccb814015dc87b66
Pallavi <pallavidule@gmail.com>
2025-04-30 16:08:51 +0200

    sliding window

--- set/medium/longest-substring-without-duplicate/solution.js
+++ set/medium/longest-substring-without-duplicate/solution.js
@@ -1,4 +1,4 @@
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring1 = function(s) {
     let set = new Set();
     let left = 0;
     let maxLength = 0;
@@ -14,9 +14,24 @@ var lengthOfLongestSubstring = function(s) {
     return maxLength;
 };
 
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for(let right = 0; right < s.length; right++){
+        while(set.has(s[right])) { // s[4] = a
+            set.delete(s[left]); // s[0] = a;
+            left++;
+        }
+        set.add(value);
+        maxLength = Math.max(maxLength, right - left + 1); // 4-0+1; // here +1 because we do left++;
+    }
+    return maxLength;
+};
+
 
 // Longest Substring Without Repeating Characters
-
 export function run() {
     const input = "abcabcbb";
     console.log('input:', input, ', output:', lengthOfLongestSubstring(input)); // Output: 3
