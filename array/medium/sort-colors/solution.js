9564ce883eec1a43d45f3f04d8fab4fc44a98e6e
Pallavi <pallavidule@gmail.com>
2025-05-20 23:45:18 +0200

    sort colors

---
+++ array/medium/sort-colors/solution.js
@@ -0,0 +1,11 @@
+const sortColors = (arr) => {
+
+}
+
+export function run() {
+    // it only has 0,1,2
+    console.log('Sorted Colors:', sortColors([2,0,2,1,1,0])); // [0,0,1,1,2,2]
+    console.log('-------------------------------------------');
+    console.log('Sorted Colors:', sortColors([2,0,1])); // [0,1,2]
+    console.log('-------------------------------------------');
+}
