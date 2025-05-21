5978c0f6961cb9afab435c40efed74a117b7189f
Pallavi <pallavidule@gmail.com>
2025-05-21 14:36:42 +0200

    readme

--- array/medium/sort-colors/solution.js
+++ array/medium/sort-colors/solution.js
@@ -80,4 +80,6 @@ export function run() {
     console.log('-------------------------------------------');
     console.log('Sorted Colors:', sortColors([2,0,1])); // [0,1,2]
     console.log('-------------------------------------------');
+    console.log('Sorted Colors:', sortColors([2,0,1,0,1,2,1,2,1])); // [0,0,1,1,1,1,2,2,2]
+    console.log('-------------------------------------------');
 }
