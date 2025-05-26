60a28eda72f4172ab85aa94289c69b07db8664db
Pallavi <pallavidule@gmail.com>
2025-05-26 14:54:57 +0200

    Remove All Occurrences of a Substring

--- string/medium/remove-all-occurrences-of-substring/solution.js
+++ string/medium/remove-all-occurrences-of-substring/solution.js
@@ -12,17 +12,35 @@ var removeOccurrences1 = function(s, part) {
 };
 
 var removeOccurrences = function(s, part) {
-    let result = "";
+    let result = ""; // using this string as stack
     for(let i=0; i< s.length; i++) {
         result += s[i];
 
-        if(result.endsWith(part)) {
+        // if(result.slice(result.length-part.length, result.length) === part)
+        // if(result.slice(-part.length) === part)
+        if(result.endsWith(part)) { 
             result = result.slice(0, result.length-part.length);
         }
     }
 
     return result;
 };
+
+var removeOccurrences = function(s, part) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        stack.push(s[i]);
+
+        if (stack.length >= part.length && 
+            stack.slice(-part.length).join('') === part) { 
+            stack.length -= part.length; // pop part.length elements
+        }
+    }
+
+    return stack.join('');
+};
+
 
 export function run() {
     console.log('String after removing All Occurrences of a Substring:', removeOccurrences("daabcbaabcbc", "abc"));  // dab  
