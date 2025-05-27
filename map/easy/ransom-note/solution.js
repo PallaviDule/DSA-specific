afb3e89cc3e2b94dff03693ee6d3b94eb98c558b
Pallavi <pallavidule@gmail.com>
2025-05-27 20:37:16 +0200

    ransom note

--- map/easy/ransom-note/solution.js
+++ map/easy/ransom-note/solution.js
@@ -3,7 +3,27 @@
  * @param {string} magazine
  * @return {boolean}
  */
+var canConstruct1 = function(ransomNote, magazine) {
+    const counts = Array(26).fill(0);
+    for (let char of magazine) {
+        // charCodeAt(): This method returns the Unicode code point (numeric representation) of the character at the specified position in the string.
+        counts[char.charCodeAt(0) - 97]++; // for a: 97, b: 98, c:99 .....
+        //  for char a: char.charCodeAt(0) = 97; counts[97-97] which is counts[0]++ : 0++; counts = [1, 0, 0]
+    }
+    for (let char of ransomNote) {
+        if (--counts[char.charCodeAt(0) - 97] < 0) return false;
+    }
+    return true;
+};
+
 var canConstruct = function(ransomNote, magazine) {
+    for (const char of magazine) {
+        ransomNote = ransomNote.replace(char, "");
+    }
+    return ransomNote.length === 0;
+};
+
+var canConstruct2 = function(ransomNote, magazine) {
     const magazineMap = new Map();
 
     // Count each character in magazine
