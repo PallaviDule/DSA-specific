3501a55565a11bf31ca5d8f40dbf42b0376eaef4
Pallavi <pallavidule@gmail.com>
2025-06-01 23:30:35 +0200

    Spiral Matrix

--- array/medium/spiral-matrix/solution.js
+++ array/medium/spiral-matrix/solution.js
@@ -1,15 +1,19 @@
+const spiralMatrix = (matrix) => {
+
+}
+
 export function run(){
     let matrix = [
                     [1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 9]
                 ];
-    console.log('Sum of Matrix diagonals', searchMatrix(matrix, 4) );
+    console.log('Spiral Matrix:', searchMatrix(matrix) );
     console.log('-------------------------------------------------------------------');
     matrix = [[1]];
-    console.log('Sum of Matrix diagonals', searchMatrix(matrix, 1) );
+    console.log('Spiral Matrix:', searchMatrix(matrix));
     console.log('-------------------------------------------------------------------');
     matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
-    console.log('Sum of Matrix diagonals', searchMatrix(matrix, 13) );
+    console.log('Spiral Matrix:', searchMatrix(matrix) );
     console.log('-------------------------------------------------------------------');
 }
